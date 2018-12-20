5c273ba88c464c6fa72a2326f256a2bb60394c72
jk00jl00 <jk00jl00@gmail.com>
2018-12-20 21:27:40 +0100

    package update

--- js/client.js
+++ js/client.js
@@ -8,40 +8,64 @@ Client.socket = io.connect();
 var player = {};
 var canvas = document.getElementById('game');
 var ctx = canvas.getContext("2d");
-
-Client.sendTest = function(){
-    console.log("test sent");
-    Client.socket.emit('test');
-};
+var screenWidth = canvas.width,
+    screenHeight = canvas.height,
+    gridWidth = screenWidth/100,
+    gridHeight = screenHeight/100,
+    mapSize = 0,
+    cameraOfSet = {x: 0, y: 0};
 
 document.onkeydown = (ev) =>{
     Client.socket.emit('keyDown', ev.keyCode);
 }
 
-Client.socket.on('allplayers',function(data){
+Client.socket.on('allplayers',function(data, size){
     player = data;
+    mapSize = size;
 });
 Client.socket.on('update',function(data){
     draw(data);
 });
 
-function draw(data){
-    console.log(data);
+function camera(){
+    if(player.x * gridWidth > screenWidth/2){
+        cameraOfSet.x = player.x * gridWidth - screenWidth/2;
+        cameraOfSet.x = (cameraOfSet.x < mapSize * gridWidth - screenWidth)? cameraOfSet.x : mapSize * gridWidth - screenWidth; 
+    } else{
+        cameraOfSet.x = 0;
+    }
+    if(player.y * gridHeight > screenHeight/2){
+        cameraOfSet.y = player.y * gridHeight - screenHeight/2;
+        cameraOfSet.y = (cameraOfSet.y < mapSize * gridHeight - screenWidth)? cameraOfSet.y : mapSize * gridHeight - screenWidth; 
+    } else{
+        cameraOfSet.y = 0;
+    }
+}
 
-    ctx.clearRect(0,0, 800, 600);
+function draw(data){
+
+    data.players.forEach((p) => {
+        if(p.id === player.id){
+            player = p;
+        }
+    });
+
+    camera();
+
+    ctx.clearRect(0,0, screenWidth, screenHeight);
     data.players.forEach((p) => {
         if(p.id === player.id)
             ctx.fillStyle = 'black';
         else
             ctx.fillStyle = 'blue';
-        ctx.fillRect(p.x * 20, p.y * 20, 20, 20);
+        ctx.fillRect(p.x * gridWidth - cameraOfSet.x, p.y * gridHeight - cameraOfSet.y, gridWidth, gridHeight);
         p.tail.forEach((t) =>{
-            ctx.fillRect(t.x * 20, t.y * 20, 20, 20);
+            ctx.fillRect(t.x * gridWidth - cameraOfSet.x, t.y * gridHeight - cameraOfSet.y, gridWidth, gridHeight);
         });
     });
     ctx.fillStyle = 'red';
     data.apples.forEach((a) =>{
-        ctx.fillRect(a.x * 20, a.y * 20, 20, 20);
+        ctx.fillRect(a.x * gridWidth - cameraOfSet.x, a.y * gridHeight - cameraOfSet.y, gridWidth, gridHeight);
     })
 }
 
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ var DIRS = {
     left: 2,
     up: 3 
 };
+var gridSize = 250;
 const KEYS = {
   up: 40,
   right: 39,
@@ -28,27 +29,12 @@ const KEYS = {
   left: 37
 };
 var players = [];
-var apples = [{x: randomInt(0,30), y: randomInt(0,30), respawn : function(){
-    this.x = randomInt(0,30);
-    this.y = randomInt(0,30);   
-    }
-},
-{x: randomInt(0,30), y: randomInt(0,30), respawn : function(){
-    this.x = randomInt(0,30);
-    this.y = randomInt(0,30);   
-    }
-},
-{x: randomInt(0,30), y: randomInt(0,30), respawn : function(){
-    this.x = randomInt(0,30);
-    this.y = randomInt(0,30);   
-    }
-},
-{x: randomInt(0,30), y: randomInt(0,30), respawn : function(){
-    this.x = randomInt(0,30);
-    this.y = randomInt(0,30);   
-    }
-}];
-var gridSize = 30;
+var apples = [];
+for(let i = 0; i < (gridSize * gridSize)/500; i++)
+    apples.push({x: randomInt(0,gridSize), y: randomInt(0,gridSize), respawn : function(){
+    this.x = randomInt(0,gridSize);
+    this.y = randomInt(0,gridSize);   
+    }});
 
 
 
@@ -57,99 +43,113 @@ server.listen(3000,function(){
 });
 
 io.on('connection',function(socket){
-    socket.player = {
-        con: socket.request.connection.remoteAddress,
-        id: lastPlayderID++,
-        x: randomInt(0,20),
-        y: randomInt(0,20),
-        dir: randomInt(0,3),
-        tail: [],
-        move : function(){
-            // Update tail
-            for(var i = this.tail.length-1; i >= 0; i--) {
-              this.tail[i].x = (i===0) ? this.x : this.tail[i-1].x;
-              this.tail[i].y = (i===0) ? this.y : this.tail[i-1].y;
-            }
-
-            // Move head
-            switch(this.dir) {
-              case DIRS.right:
-                this.x++; break;
-              case DIRS.left:
-                this.x--; break;
-              case DIRS.down:
-                this.y--; break;
-              case DIRS.up:
-                this.y++; break;
-            }
-
-            // Check boundaries
-            if(this.x > gridSize-1) this.x = 0;
-            if(this.x < 0) this.x = gridSize-1;
-            if(this.y > gridSize-1) this.y = 0;
-            if(this.y < 0) this.y = gridSize-1;
-
-            // Collission detection
-            this.checkCollisions();
-        },
-        checkCollisions: function() {
-            // With other snakes (including ours)
-            players.forEach((s) => {
-              // Heads except ourself
-              if(s !== this) {
-                if(s.x === this.x && s.y === this.y) {
-                  // The bigger survives
-                  // ToDo: 3 outcomes
-                  // - Same length = both die
-                  if(s !== this && this.tail.length < s.tail.length) {
-                    this.respawn();
-                  } else {
-                    for(let i = 0; i < s.tail.length + 1; i++){
-                        this.addTail();
-                    }
-                    s.respawn();
-                  }
-                }
-              }
-              // Tails
-              if(s !== this)
-                  s.tail.forEach((t) => {
-                    if(t.x === this.x && t.y === this.y) {    
-                        let a = s.tail.splice(s.tail.indexOf(t));
-                        console.log(a);
-                        for(let i = 0; i < a.length + 1; i++){
-                            this.addTail();
-                        }
-                        
-                        s.tail.splice(s.tail.indexOf(t));
-                      
-                    }
-                  });
-            });
-            // With apples
-            apples.forEach((a) => {
-              if(a.x === this.x && a.y === this.y) {
-                this.addTail();
-                a.respawn();
-              }
-            });
-        },
-        respawn : function(){
-            this.x = randomInt(0,20);
-            this.y = randomInt(0,20);
-            this.dir = randomInt(0,3);
-            this.tail = [];
-            socket.player.addTail();
-            socket.player.addTail();
-        },
-        addTail : function(){
-            this.tail.push({x: this.x, y: this.y});
+    var i;
+    let isConnected = false;
+    players.forEach((p)=>{
+        if(p.con == socket.request.connection.remoteAddress){
+            socket.player = p; 
+            isConnected = true;
         }
-    };
-    socket.player.addTail();
-    socket.player.addTail();
-    players.push(socket.player);
-    socket.emit('allplayers', socket.player);
+    });
+    if(!isConnected){
+        socket.player = {
+            con: socket.request.connection.remoteAddress,
+            id: lastPlayderID++,
+            x: randomInt(0,gridSize),
+            y: randomInt(0,gridSize),
+            dir: randomInt(0,3),
+            tail: [],
+            move : function(){
+                // Update tail
+                for(var i = this.tail.length-1; i >= 0; i--) {
+                  this.tail[i].x = (i===0) ? this.x : this.tail[i-1].x;
+                  this.tail[i].y = (i===0) ? this.y : this.tail[i-1].y;
+                }
+
+                // Move head
+                switch(this.dir) {
+                  case DIRS.right:
+                    this.x++; break;
+                  case DIRS.left:
+                    this.x--; break;
+                  case DIRS.down:
+                    this.y--; break;
+                  case DIRS.up:
+                    this.y++; break;
+                }
+
+                // Check boundaries
+                if(this.x > gridSize-1) this.x = 0;
+                if(this.x < 0) this.x = gridSize-1;
+                if(this.y > gridSize-1) this.y = 0;
+                if(this.y < 0) this.y = gridSize-1;
+                this.checkCollisions();
+
+            },
+            checkCollisions: function() {
+                
+                this.tail.forEach((t) =>{
+                    if(t.x === this.x && t.y === this.y){
+                        this.tail.splice(this.tail.indexOf(t));
+                    }
+                });
+
+                apples.forEach((a) => {
+                    if(a.x === this.x && a.y === this.y){
+                        this.addTail(2);
+                        a.respawn();
+                    }
+                });
+
+                players.forEach((s) => {
+
+                    //Collide with other heads
+                    if(s !== this){
+                        if(s !== this && s.x === this.x && s.y === this.y){
+                            if(s.tail.length > this.tail.length){
+                                for(let i = 0; i < this.tail.length + 2; i++){
+                                    s.addTail();
+                                }
+                                this.respawn();
+                            } else{
+                                for(let i = 0; i < s.tail.length + 2; i++){
+                                    this.addTail();
+                                }
+                                s.respawn();
+                            }
+                        }
+
+                        s.tail.forEach((t) =>{
+                            if(t.x === this.x && t.y === this.y){
+                                for(let i = 0; i <= s.tail.length - s.tail.indexOf(t); i++){
+                                    this.addTail();
+                                }
+                                s.tail.splice(s.tail.indexOf(t));
+                            }
+                        });
+                    }
+
+                });
+            },
+            respawn : function(){
+                this.x = randomInt(0,gridSize);
+                this.y = randomInt(0,gridSize);
+                this.dir = randomInt(0,3);
+                this.tail = [];
+                this.addTail(3);
+            },
+            addTail : function(l){
+                if(!l) l = 1;
+                while(l--)
+                    this.tail.push({x: this.x, y: this.y});
+            }
+        };
+        
+        socket.player.addTail(3);
+        
+        players.push(socket.player);
+    }
+    socket.emit('allplayers', socket.player, gridSize);
 
     socket.on('keyDown',function(key){
         switch (key) {
@@ -169,23 +169,9 @@ io.on('connection',function(socket){
     });
 
     socket.on('disconnect',function(){
-        console.log("bop");
         let i = players.indexOf(socket.player);
         players.splice(i, 1);
     });
-
-    socket.on('test',function(){
-        console.log('test received');
-    });
-    console.log(socket.request.connection.remoteAddress);
-    let sockets = []
-    players.forEach((p)=>{
-        if(sockets.includes(p.con)){
-            socket.disconnect();
-            console.log("Bip");
-        }
-        sockets.push(p.con);
-    }) 
 });
 
 
